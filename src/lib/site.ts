fcb98e9cf75f3dc4703d6f39181b4ae90b0189d8
Jacob Hilker <jacob.hilker2@gmail.com>
2023-07-15 15:48:17 -0400

    created SiteConfig type in site.ts

--- src/lib/site.ts
+++ src/lib/site.ts
@@ -1,6 +1,11 @@
-export const SITE_META: Object = {
-    title: "Jacob's Website",
-    description: "The blog and website of Jacob Hilker.",
-    author: "Jacob Hilker"
-};
+type SiteConfig = {
+        title: string,
+        description: string,
+        author: string
+}
 
+export const SITE_META: SiteConfig = {
+        title: "Jacob's Website",
+        description: "The personal website and blog of Jacob Hilker.",
+        author: "Jacob Hilker"
+}
