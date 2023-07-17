4f34ceb8b8003de8e3288341f6ae1d98c3da83fb
Jacob Hilker <jacob.hilker2@gmail.com>
2023-07-17 16:38:28 -0400

    added categories, tags, lastMod, and series to blogCollection

--- src/content/config.ts
+++ src/content/config.ts
@@ -5,7 +5,14 @@ const blogCollection = defineCollection({
     schema: z.object({
         title: z.string({required_error: "A title is required for a blog post."}),
         draft: z.boolean({required_error: "Every post is either a draft or not."}).default(true),
-        pubDate: z.date({required_error: "You need to have a date when the post was published."})
+        pubDate: z.date({required_error: "You need to have a date when the post was published."}),
+        tags: z.array(z.string()).optional(),
+        categories: z.array(z.string()).optional(),
+        lastModDate: z.date().optional(),
+        series: z.object({
+            name: z.string({required_error: "Any series must have a name."}),
+            weight: z.number({required_error: "Any post in a series must have a weight to sort by in that series."}),
+        }).optional(),
     })
 })
 
