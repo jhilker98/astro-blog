b8246225a1679b5658762817745a2e34648bec25
Jacob Hilker <jacob.hilker2@gmail.com>
2023-07-17 13:56:50 -0400

    created type SiteLink, added Icons to fixed-sidebar

--- src/lib/site.ts
+++ src/lib/site.ts
@@ -1,14 +1,26 @@
 
+type SocialLink = {
+        icon: string,
+        href: string
+}
+
 type SiteConfig = {
         title: string,
         description: string,
-        author: string
+        author: string,
+        availableForHire: boolean,
+        socialLinks: SocialLink[]
 }
+
 
 export const SITE_META: SiteConfig = {
         title: "Jacob's Website",
         description: "The personal website and blog of Jacob Hilker.",
-        author: "Jacob Hilker"
+        author: "Jacob Hilker",
+        availableForHire: true,
+        socialLinks: [
+                {icon: "devicon:github", href: "https://github.com/jhilker98"}
+        ]
 }
 
 export const NAV_LINKS = [
