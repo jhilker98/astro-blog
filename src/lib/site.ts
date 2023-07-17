74b6150288b1f0fd5ae59a78b541c1cdbcdfef4c
Jacob Hilker <jacob.hilker2@gmail.com>
2023-07-17 16:32:15 -0400

    added slugifyPostDates to site.ts

--- src/lib/site.ts
+++ src/lib/site.ts
@@ -28,6 +28,20 @@ export const NAV_LINKS = [
         { href: "/resume/", name: "Resumé"}
 ];
 
+export function slugifyPostDates(d: Date): string {
+  /**
+   * A function to return dates in the form of `/year/month`.
+   * @param {Date} d - The date of the post.
+   * @returns {string} String in the format of `/year/month`.
+   */
+  let f = {
+    year: new Intl.DateTimeFormat("en-US", { year: "numeric" }),
+    month: new Intl.DateTimeFormat("en-US", { month: "2-digit" }),
+  };
+
+  return `${f.year.format(d)}/${f.month.format(d)}`;
+}
+
 export function formatDate(date: Date){
         return new Date(date).toLocaleDateString("en-GB", { 
                 timeZone: "UTC",
