330867c4013e8b9e7dce215bb90321af9cf4d731
Jacob Hilker <jacob.hilker2@gmail.com>
2023-07-17 12:33:25 -0400

    added sitemap and SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,19 @@
 import { defineConfig } from "astro/config";
-
 import alpinejs from "@astrojs/alpinejs";
 import tailwind from "@astrojs/tailwind";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [alpinejs(), tailwind()],
+  integrations: [alpinejs(), tailwind(), sitemap()],
   markdown: {
     drafts: true,
     shikiConfig: {
-      theme: "material-theme-darker",
-    },
+      theme: "material-theme-darker"
+    }
   },
   experimental: {
-    assets: true,
-  },
+    assets: true
+  }
 });
