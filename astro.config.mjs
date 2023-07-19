d169f27997465ee681931afdb9f46b7599c759da
Jacob Hilker <jacob.hilker2@gmail.com>
2023-07-19 15:52:47 -0400

    added mdx and resume.mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from "astro/config";
 import alpinejs from "@astrojs/alpinejs";
 import tailwind from "@astrojs/tailwind";
-
 import sitemap from "@astrojs/sitemap";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [alpinejs(), tailwind(), sitemap()],
+  integrations: [alpinejs(), tailwind(), sitemap(), mdx()],
   markdown: {
     drafts: true,
     shikiConfig: {
