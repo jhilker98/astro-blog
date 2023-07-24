52d700b4ef25eb5ff81bdee0b3b625c15fc25a6f
Jacob Hilker <jacob.hilker2@gmail.com>
2023-07-24 13:45:24 -0400

    added lint command to package.json

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,48 +1,47 @@
 type SocialLink = {
-        icon: string,
-        href: string
-}
+  icon: string;
+  href: string;
+};
 
 type SiteConfig = {
-        title: string,
-        description: string,
-        author: string,
-        tagline: string,
-        availableForHire: boolean,
-        socialLinks: SocialLink[]
-}
-
+  title: string;
+  description: string;
+  author: string;
+  tagline: string;
+  availableForHire: boolean;
+  socialLinks: SocialLink[];
+};
 
 export const SITE_META: SiteConfig = {
-        title: "Jacob's Website",
-        description: "The personal website and blog of Jacob Hilker.",
-        author: "Jacob Hilker",
-        availableForHire: true,
-        tagline: "A front-end developer, who enjoys using Emacs, Hugo, Python, and Tailwind.",
-        socialLinks: [
-                { icon: "fa6-brands:github", href: "https://github.com/jhilker98"},
-                { icon: "fa6-brands:gitlab", href: "https://gitlab.com/jhilker98" },
-                { icon: "fa6-brands:linkedin", href: "https://linkedin.com/in/jhilker"},
-                { icon: "fa6-brands:codepen", href: "https://codepen.io/hilkerj/"}
-        ]
-}
+  title: "Jacob's Website",
+  description: "The personal website and blog of Jacob Hilker.",
+  author: "Jacob Hilker",
+  availableForHire: true,
+  tagline:
+    "A front-end developer, who enjoys using Emacs, Hugo, Python, and Tailwind.",
+  socialLinks: [
+    { icon: "fa6-brands:github", href: "https://github.com/jhilker98" },
+    { icon: "fa6-brands:gitlab", href: "https://gitlab.com/jhilker98" },
+    { icon: "fa6-brands:linkedin", href: "https://linkedin.com/in/jhilker" },
+    { icon: "fa6-brands:codepen", href: "https://codepen.io/hilkerj/" },
+  ],
+};
 
 interface NavLink {
-        href: string,
-        name: string,
-        children?: NavLink[]
+  href: string;
+  name: string;
+  children?: NavLink[];
 }
 
-export function isNavLinkActive(p1: string, p2: string) : boolean {
-        //let page = new URL(`${Astro.url.origin}/${p1}`).slice(1).slice(1);
-        return p1 === p2;
+export function isNavLinkActive(p1: string, p2: string): boolean {
+  //let page = new URL(`${Astro.url.origin}/${p1}`).slice(1).slice(1);
+  return p1 === p2;
 }
-
 
 export const NAV_LINKS: NavLink[] = [
-        { href: "/", name: "Home"},
-        { href: "/resume/", name: "Resumé"},
-        { href: "/blog/", name: "Blog"}
+  { href: "/", name: "Home" },
+  { href: "/resume/", name: "Resumé" },
+  { href: "/blog/", name: "Blog" },
 ];
 
 export function slugifyPostDates(d: Date): string {
@@ -59,11 +58,11 @@ export function slugifyPostDates(d: Date): string {
   return `${f.year.format(d)}/${f.month.format(d)}`;
 }
 
-export function formatDate(date: Date){
-        return new Date(date).toLocaleDateString("en-GB", { 
-                timeZone: "UTC",
-                day: "numeric",
-                month: "short",
-                year: "numeric",
-        });
+export function formatDate(date: Date) {
+  return new Date(date).toLocaleDateString("en-GB", {
+    timeZone: "UTC",
+    day: "numeric",
+    month: "short",
+    year: "numeric",
+  });
 }
