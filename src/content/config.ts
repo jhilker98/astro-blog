05b86a816ed935ee9e7826d64c192d938ffb7881
Jacob Hilker <jacob.hilker2@gmail.com>
2023-07-24 13:34:49 -0400

    added featured parameter to blog collection

--- src/content/config.ts
+++ src/content/config.ts
@@ -9,6 +9,7 @@ const blogCollection = defineCollection({
         tags: z.array(z.string()).optional(),
         categories: z.array(z.string()).optional(),
         lastModDate: z.date().optional(),
+        featured: z.boolean({invalid_type_error: "A post is either featured or not."}).default(false),
         series: z.object({
             name: z.string({required_error: "Any series must have a name."}),
             weight: z.number({required_error: "Any post in a series must have a weight to sort by in that series."}),
